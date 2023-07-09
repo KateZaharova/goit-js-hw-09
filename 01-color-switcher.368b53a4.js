df130988815fbdd8fff4ae60460e8cb703c4add0
KateZaharova <124520175+KateZaharova@users.noreply.github.com>
2023-07-09 15:23:39 +0000

    Deploying to gh-pages from @ KateZaharova/goit-js-hw-09@8af742f4db98a442a63b3ed30550b453ccbced1d 🚀

---
+++ 01-color-switcher.368b53a4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=(document.querySelector("button"),null);e.disabled=!0,t.addEventListener("click",(function(a){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,t===a.target&&(e.disabled=!1)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.368b53a4.js.map
