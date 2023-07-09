1d4841797b9cb71f96ee15c11daeab01bafe4860
Kate Zaharova <15071981ktz@gmail.com>
2023-07-09 17:51:37 +0300

    task-01

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,34 @@
+const start = document.querySelector("button[data-start]");
+const stop = document.querySelector("button[data-stop]");
+const body = document.querySelector("body");
+const button = document.querySelector("button")
+
+let timerId = null;
+stop.disabled = true;
+
+start.addEventListener("click", (event) => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+      start.disabled = true;
+    if (event.target = start) {
+        return stop.disabled = false;
+    }
+})
+
+stop.addEventListener("click", () => {
+    clearInterval(timerId);
+    start.disabled = false;
+    stop.disabled = true;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+
+
+
+
+
